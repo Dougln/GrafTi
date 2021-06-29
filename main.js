bab2b78e485bd2b7d4cca48bcd69005d14a91fed
Dougln <douglasmichel31@gmail.com>
2021-06-28 22:56:43 -0300

    Aula 2: 1.37

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-alert("Eu existo")
+/*
+* Objetivo: adicionar e fechar os menus quando clicar no ícone
+*/ 
+const nav = document.querySelector("#header nav")
+const toggle = document.querySelectorAll("nav .toggle")
+
+for (const element of toggle) {
+  element.addEventListener("click",function(){
+    nav.classList.toggle('show')
+  })
+}
+/*
+* Objetivo: quando clicar nos links, redirecionar para suas respectivas "sections"
+*/ 
+const begin = document.querySelectorAll("nav ul li a")
+console.log(begin);
+for (const element of begin) {
+  element.addEventListener("click", function(){
+    nav.classList.remove("show")
+  })
+}
